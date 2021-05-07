6c14d61b154e73fa1121f4dc4dfebf8f215d5dfb
TiyaAnlite <chenquanyu1999@126.com>
2021-05-08 02:51:56 +0800

    封装目前所有底层库支持的接口，包括哈希与哈希认证，加解密和数据读写

--- pyViKey.py
+++ pyViKey.py
@@ -15,15 +15,18 @@ def vikey_exception_wrapper(func):
     return exception_wrapper
 
 
-@enum.unique
-class ViKeyIdentity(enum.Enum):
+class Identity(enum.Enum):
+    """身份状态信息，默认为anonymous权限，部分操作对权限有最低要求\n
+    ViKeyIdentity.anonymous: 匿名权限\n
+    ViKeyIdentity.user: 用户权限\n
+    ViKeyIdentity.admin: 管理员权限"""
     anonymous = 0
     user = 1
     admin = 2
 
 
-@enum.unique
-class ViKeyType(enum.Enum):
+class Type(enum.Enum):
+    """标识硬件类型"""
     ViKeyAPP = 0
     ViKeySTD = 1
     ViKeyNET = 2
@@ -35,8 +38,8 @@ class ViKeyType(enum.Enum):
     ViKeyInvalid = 0x0C
 
 
-@enum.unique
-class ViKeyTypeCN(enum.Enum):
+class TypeCN(enum.Enum):
+    """标识硬件类型(中文)"""
     ViKeyAPP = "实用型加密狗"
     ViKeySTD = "标准型加密狗"
     ViKeyNET = "网络型加密狗"
@@ -48,8 +51,8 @@ class ViKeyTypeCN(enum.Enum):
     ViKeyInvalid = "无效类型"
 
 
-@enum.unique
-class ViKeyErrorType(enum.Enum):
+class ErrorType(enum.Enum):
+    """底层库错误码标识"""
     VIKEY_SUCCESS = 0x00000000
     VIKEY_ERROR_NO_VIKEY = 0x80000001
     VIKEY_ERROR_INVALID_PASSWORD = 0x80000002
@@ -77,10 +80,11 @@ class ViKeyErrorType(enum.Enum):
     VIKEY_ERROR_UNKNOWN_ERROR = 0xFFFFFFFF
     # Custom Error
     VIKEY_CHANGED = 0x90000001
+    VIKEY_CROSS_REGIONAL_WRITE = 0x90000002
 
 
-@enum.unique
-class ViKeyErrorTypeCN(enum.Enum):
+class ErrorTypeCN(enum.Enum):
+    """底层库错误码标识(中文)"""
     VIKEY_SUCCESS = "成功"
     VIKEY_ERROR_NO_VIKEY = "没有找到ViKey加密锁"
     VIKEY_ERROR_INVALID_PASSWORD = "密码错误"
@@ -105,43 +109,62 @@ class ViKeyErrorTypeCN(enum.Enum):
     VIKEY_ERROR_NO_SUPPORT = "当前ViKey加密锁不支持此功能"
     VIKEY_ERROR_CATCH = "发生异常"
     VIKEY_ERROR_UNKNOWN_ERROR = "未知错误"
+    # Custom Error
     VIKEY_CHANGED = "当前ViKey已被改变"
+    VIKEY_CROSS_REGIONAL_WRITE = "跨用户区写入数据"
+
+
+class StorageInfo(enum.Enum):
+    """ViKey存储空间信息帮助类(单位为字节)"""
+    ViKeyAPP = 128
+    ViKeySTD = 2048
+    ViKeyNET = 2048
+    ViKeyPRO = 4096
+    ViKeyWEB = 128
+    ViKeyTIME = 256
 
 
 class ViKeyError(RuntimeError):
+    """底层库错误的高级封装，提供友好状态信息显示"""
 
     def __init__(self, errcode):
         try:
-            t = ViKeyErrorType(errcode)
+            t = ErrorType(errcode)
         except ValueError:
             errcode = 0xFFFFFFFF
-            t = ViKeyErrorType(errcode)
+            t = ErrorType(errcode)
         self.err_code = errcode
         self.err_type = t
-        self.err_msg = ViKeyErrorTypeCN[self.err_type.name]
+        self.err_msg = ErrorTypeCN[self.err_type.name]
 
     def __str__(self):
         return f"[0x{hex(self.err_code).upper()[2:]}]{self.err_msg.name}: {self.err_msg.value}"
 
 
 class PyViKeyPlus:
+    """
+    ViKey Python库高级封装，提供面向对象支持与错误包装，状态码帮助
+    """
 
     def __init__(self, index: int = 0):
+        """
+        :param index: ViKey序号
+        """
         find()
         self.index = index
         self.HID = 0
         self.softHID = ""
         self.type = None
-        self.level = ViKeyIdentity.anonymous
+        self.level = Identity.anonymous
         self.__info_init()
 
     def __info_init(self):
         try:
             self.HID = _Lib.VikeyGetHID(self.index)
             self.softHID = _Lib.VikeyGetSoftIDString(self.index)
-            t = ViKeyType(_Lib.VikeyGetType(self.index))
-            self.type = (t.value, t.name, ViKeyTypeCN[t.name].value)
-            self.level = ViKeyIdentity(_Lib.VikeyGetLevel(self.index))
+            t = Type(_Lib.VikeyGetType(self.index))
+            self.type = (t.value, t.name, TypeCN[t.name].value)  # (typeId, typeName, typeNameCN)
+            self.level = Identity(_Lib.VikeyGetLevel(self.index))
         except RuntimeError as err:
             raise ViKeyError(err.args[0])
 
@@ -155,58 +178,188 @@ class PyViKeyPlus:
         if self.HID != _Lib.VikeyGetHID(self.index):
             raise RuntimeError(0x90000001)
 
-    def __get_level(self):
+    def __get_level(self) -> Identity:
         self.__check()
-        self.level = ViKeyIdentity(_Lib.VikeyGetLevel(self.index))
+        self.level = Identity(_Lib.VikeyGetLevel(self.index))
         return self.level
 
-    def __login_user(self, password: str):
+    def __get_name(self) -> str:
         self.__check()
-        self.level = ViKeyIdentity(_Lib.VikeyUserLogin(password, self.index))
+        if Identity.user.value > self.__get_level().value:  # 权限检查
+            self.__login_user(self.softHID)
+        return _Lib.VikeyGetPtroductName(self.index)
+
+    def __login_user(self, password: str) -> Identity:
+        self.__check()
+        self.level = Identity(_Lib.VikeyUserLogin(password, self.index))
         return self.level
 
-    def __login_admin(self, password: str):
+    def __login_admin(self, password: str) -> Identity:
         self.__check()
-        self.level = ViKeyIdentity(_Lib.VikeyAdminLogin(password, self.index))
+        self.level = Identity(_Lib.VikeyAdminLogin(password, self.index))
         return self.level
 
-    def __logout(self):
+    def __logout(self) -> Identity:
         self.__check()
-        self.level = ViKeyIdentity(_Lib.VikeyLogout(self.index))
+        self.level = Identity(_Lib.VikeyLogout(self.index))
         return self.level
 
     @vikey_exception_wrapper
-    def get_level(self):
+    def get_level(self) -> Identity:
+        """获取当前登录权限"""
         return self.__get_level()
 
     @vikey_exception_wrapper
-    def login(self, identity: ViKeyIdentity = ViKeyIdentity.user, password: str = None):
+    def get_name(self) -> str:
+        """获取硬件名"""
+        return self.__get_name()
+
+    @vikey_exception_wrapper
+    def login(self, identity: Identity = Identity.user, password: str = None):
+        """
+        登录硬件为指定权限\n
+        :param identity: ViKeyIdentity权限类
+        :param password: 密码
+        """
         if not password:
             password = self.softHID
         self.__check()
-        if identity == ViKeyIdentity.anonymous:
-            self.level = ViKeyIdentity(self.__logout())
-        elif identity == ViKeyIdentity.user:
-            self.level = ViKeyIdentity(self.__login_user(password))
-        elif identity == ViKeyIdentity.admin:
-            self.level = ViKeyIdentity(self.__login_admin(password))
+        if identity == Identity.anonymous:
+            self.level = Identity(self.__logout())
+        elif identity == Identity.user:
+            self.level = Identity(self.__login_user(password))
+        elif identity == Identity.admin:
+            self.level = Identity(self.__login_admin(password))
         return self.level
 
     @vikey_exception_wrapper
     def logout(self):
+        """登出硬件，返回匿名状态"""
         return self.__logout()
 
     @vikey_exception_wrapper
-    def random_number(self):
+    def get_random_number(self) -> tuple:
+        """返回一组随机数(4个)"""
         self.__check()
         return _Lib.ViKeyRandom(self.index)
 
     @vikey_exception_wrapper
     def random_number_generator(self):
+        """随机数生成器"""
         while True:
             self.__check()
             for n in _Lib.ViKeyRandom(self.index):
                 yield n
+
+    @vikey_exception_wrapper
+    def md5(self, data: bytes) -> bytes:
+        """
+        MD5哈希算法\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeyMD5(data, self.index)
+
+    @vikey_exception_wrapper
+    def sha1(self, data: bytes) -> bytes:
+        """
+        SHA1哈希算法\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeySHA1(data, self.index)
+
+    @vikey_exception_wrapper
+    def hmac_md5(self, data: bytes) -> bytes:
+        """
+        HMAC哈希验证算法(MD5)\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeyHmacMD5(data, self.index)
+
+    @vikey_exception_wrapper
+    def hmac_sha1(self, data: bytes) -> bytes:
+        """
+        HMAC哈希验证算法(SHA1)\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeyHmacSHA1(data, self.index)
+
+    @vikey_exception_wrapper
+    def encrypt_des(self, data: bytes) -> bytes:
+        """
+        DES对称加密\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeyDesEncrypt(data, self.index)
+
+    @vikey_exception_wrapper
+    def decrypt_des(self, data: bytes) -> bytes:
+        """
+        DES对称解密\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.VikeyDesDecrypt(data, self.index)
+
+    @vikey_exception_wrapper
+    def encrypt_3des(self, data: bytes) -> bytes:
+        """
+        3DES对称加密\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.Vikey3DesEncrypt(data, self.index)
+
+    @vikey_exception_wrapper
+    def decrypt_3des(self, data: bytes) -> bytes:
+        """
+        3DES对称解密\n
+        :param data: 输入数据
+        :return: 输出数据
+        """
+        self.__check()
+        return _Lib.Vikey3DesDecrypt(data, self.index)
+
+    @vikey_exception_wrapper
+    def read_data(self, address: int, length: int) -> bytes:
+        """
+        读取存储区数据\n
+        :param address: 起始地址
+        :param length: 长度
+        :return: 返回数据
+        """
+        self.__check()
+        return _Lib.VikeyReadData(address, length, self.index)
+
+    @vikey_exception_wrapper
+    def write_data(self, address: int, data: bytes) -> int:
+        """
+        写入存储区数据\n
+        当ViKey的为前五个支持类型时，会先进行读写区越界检查\n
+        否则将尝试直接写入\n
+        :param address: 起始地址
+        :param data: 写入数据
+        :return: 写入的数据长度
+        """
+        if self.type[0] <= Type.ViKeyTIME.value:
+            # 边界检查区域
+            user_capacity = getattr(StorageInfo, self.type[1]).value / 2
+            if address < user_capacity <= address + len(data) - 1:
+                # 写入数据超出用户区
+                raise RuntimeError(0x90000002)
+        self.__check()
+        return _Lib.VikeyWriteData(address, data, self.index)
 
 
 def find() -> int:
